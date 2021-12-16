7961be8aefe78f24c6557d1d76216ddb79bcd38f
Joshua Potter <jrpotter2112@gmail.com>
2021-12-15 20:06:03 -0500

    Add support for darwin (and others) as well.

--- flake.nix
+++ flake.nix
@@ -1,41 +1,47 @@
 {
   description = "A very basic flake";
 
-  outputs = { self, nixpkgs }:
-    let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      ruby = pkgs.ruby;
-      gems = pkgs.bundlerEnv {
-        name = "pages-env";
-        inherit ruby;
-        gemdir = self;
-      };
-    in {
-      defaultPackage.x86_64-linux = self.packages.x86_64-linux.jekyll;
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-      packages.x86_64-linux.jekyll =
-        with import nixpkgs { system = "x86_64-linux"; };
-        stdenv.mkDerivation {
-          name = "jekyll";
-          dontUnpack = true;
-          buildInputs = [ gems ];
-          installPhase = ''
-            mkdir -p $out/bin
-            bin=$out/bin/jekyll
-            cat > $bin <<EOF
-#!/bin/sh -e
-${gems}/bin/jekyll serve --watch
-EOF
-            chmod +x $bin
-          '';
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        ruby = pkgs.ruby;
+        gems = pkgs.bundlerEnv {
+          name = "pages-env";
+          inherit ruby;
+          gemdir = self;
         };
+      in {
+        defaultPackage = self.packages.${system}.jekyll;
 
-      devShell.x86_64-linux = pkgs.mkShell {
-        buildInputs = [
-          pkgs.bundix
-          pkgs.bundler
-          self.packages.x86_64-linux.jekyll
-        ];
-      };
-    };
+        packages.jekyll =
+          with import nixpkgs { inherit system; };
+          stdenv.mkDerivation {
+            name = "jekyll";
+            dontUnpack = true;
+            buildInputs = [ gems ];
+            installPhase = ''
+              mkdir -p $out/bin
+              bin=$out/bin/jekyll
+              cat > $bin <<EOF
+  #!/bin/sh -e
+  ${gems}/bin/jekyll serve --watch
+  EOF
+              chmod +x $bin
+            '';
+          };
+
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.bundix
+            pkgs.bundler
+            self.packages.${system}.jekyll
+          ];
+        };
+      });
 }
